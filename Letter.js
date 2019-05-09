46896a9f0336785a1636ecbb68250460751b1629
Kismet <47130796+charlotterice@users.noreply.github.com]>
2019-05-08 21:35:28 -0400

    started letter constructor

---
+++ Letter.js
@@ -0,0 +1,8 @@
+var letter = function Letter(character){
+    this.character = character,
+    this.guessed =function(){
+        if(this.character===true){
+            console.log(this.character);
+        }
+    }
+}
